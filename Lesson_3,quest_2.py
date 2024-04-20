c6d58bcf62e4b57e09086da5eab14332a0f892d6
maximilian007i <133326858+maximilian007i@users.noreply.github.com>
2024-04-20 21:33:17 +0300

    Первые домашние задания

---
+++ Lesson_3,quest_2.py
@@ -0,0 +1,18 @@
+# Вопрос отчего код в комменте не запустился?
+# Stage_1, Stage_2, Stage_3, Stage_4, Stage_5, Stage_6, Stage_7, Stage_8, Stage_9, Stage_10, Stage_11= input("Введите первы этап:"), input("Введите второй этап:"), input("Введите третий этап:"), input("Введите четвертый этап:"), input("Введите пятый этап:"), input("Введите шестой этап:"), input("Введите седьмой этап:"),
+#input("Введите восьмой этап:"), input("Введите девятый этап:"), input("Введите десятый этап:"), input("Введите одинадцатый этап:")
+#print(Stage_1, Stage_2, Stage_3, Stage_4, Stage_5, Stage_6, Stage_7, Stage_8, Stage_9, Stage_10, Stage_11, sep = "=>")
+
+
+Stage_1 = input("Введите первы этап:")
+Stage_2 = input("Введите второй этап:")
+Stage_3 = input("Введите третий этап:")
+Stage_4 = input("Введите четвертый этап:")
+Stage_5 = input("Введите пятый этап:")
+Stage_6 = input("Введите шестой этап:")
+Stage_7 = input("Введите седьмой этап:")
+Stage_8 = input("Введите восьмой этап:")
+Stage_9 = input("Введите девятый этап:")
+Stage_10 = input("Введите десятый этап:")
+Stage_11 = input("Введите одинадцатый этап:")
+print(Stage_1, Stage_2, Stage_3, Stage_4, Stage_5, Stage_6, Stage_7, Stage_8, Stage_9, Stage_10, Stage_11, sep = "=>")
