25bbd4f6c254a4e8b95cc6f16fc8beb17948e924
maximilian007i <133326858+maximilian007i@users.noreply.github.com>
2024-04-22 22:07:08 +0300

    Домашнее задание урок 6

---
+++ Lesson_6,quest_1.py
@@ -0,0 +1,7 @@
+a = int(input())
+count = 0
+for i in range(a):
+    k = int(input())
+    if (k==0):
+        count+=1
+print(count)
---
+++ Lesson_6,quest_3.py
@@ -0,0 +1,7 @@
+a = 2
+b = 8
+numbers = []
+for i in range(a, b):
+    if i % 2 == 0:
+        numbers.append(i)
+print(numbers)
